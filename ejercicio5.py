c483caa768c298923cfbb8512a66bb573f0470bd
Jesús Bueno <jesusba60@gmail.com>
2015-02-26 10:00:01 +0100

    Fin de ejercicio 5

--- ejercicio5.py
+++ ejercicio5.py
@@ -3,6 +3,7 @@
 #5. Mostrar todas las incidencias entre dos fechas introducidas por el usuario.
 
 from lxml import etree
+from datetime import datetime
 
 incidencias=etree.parse('http://www.dgt.es/incidenciasXY.xml')
 lista=incidencias.getroot()
@@ -10,17 +11,19 @@ lista=incidencias.getroot()
 fechainicio = raw_input("Introduzca una fecha (DD-MM-YYYY): ").upper()
 fechafin = raw_input("Introduzca otra fecha (DD-MM-YYYY): ").upper()
 
-fechainicior = fechainicio[8:10]+"-"+fechainicio[5:8]+fechainicio[0:4]
-fechafinr = fechafin[8:10]+"-"+fechafin[5:8]+fechafin[0:4]
+confechaini = datetime.strptime(fechainicio, "%d-%m-%Y")
+confechafin = datetime.strptime(fechafin, "%d-%m-%Y")
 
-for n in xrange(len(lista)):
-	if lista[n][6].text[:9] > fechainicior and fechafinr < lista[n][6].text[:9]:
+fechas=lista.findall("incidencia/fechahora_ini")
+for f in fechas:
+	confechadgt=datetime.strptime(f.text, "%Y-%m-%d %H:%M")
+	if confechadgt > confechaini and confechafin > confechadgt:
 		print " "
-		print "ID Referencia:",lista[n][13].text
-		print "Tipo de incidencia:",lista[n][0].text
-		print "Población:",lista[n][5].text 
-		print "Fecha de inicio:",lista[n][6].text 
-		print "Causa:",lista[n][4].text 
-		print "Nivel:",lista[n][7].text 
-		print "Carretera:",lista[n][8].text 
-		print "Sentido:",lista[n][11].text
+		print "ID Referencia:",f.getparent().find("ref_incidencia").text	
+		print "Tipo de incidencia:",f.getparent().find("tipo").text	
+		print "Población:",f.getparent().find("poblacion").text	
+		print "Fecha de inicio:",f.getparent().find("fechahora_ini").text	
+		print "Causa:",f.getparent().find("causa").text	
+		print "Nivel:",f.getparent().find("nivel").text	
+		print "Carretera:",f.getparent().find("carretera").text	
+		print "Sentido:",f.getparent().find("sentido").text	
